22c52932d2d0787eaed5dc4f4d992e279c954ee0
Apaar <a.sadhh02@gmail.com>
2023-12-29 23:32:40 +0530

    install tailwind css

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { metadata } from './layout'
+
+
+export default function Home() {
+  return (
+    <h1 className="text-red-400">Home</h1>
+  )
+}
