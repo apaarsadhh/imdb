980c666a26a97e9bf7810d60d84ddab529d897e5
Apaar <a.sadhh02@gmail.com>
2024-01-08 23:26:01 +0530

    Dark mode added.2

--- src/app/components/DarkModeSwitch.jsx
+++ src/app/components/DarkModeSwitch.jsx
@@ -1,23 +1,28 @@
 "use client";
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { MdLightMode } from "react-icons/md";
 import { FaMoon } from "react-icons/fa";
 import { useTheme } from 'next-themes'
+import { useState } from 'react';
+
 
 export default function DarkModeSwitch() {
     const { systemTheme, theme, setTheme } = useTheme()
+    const [mounted, setMounted] = useState(false)
 
+
+    useEffect(() => () => setMounted(true), [])
 
     const currentTheme = theme === "system" ? systemTheme : theme;
     return (
         <>
-            {currentTheme === "dark" ? (
+            {mounted && (currentTheme === "dark" ? (
                 <MdLightMode className="text-xl cursor-pointer hover:text-amber-500" onClick={() => setTheme("light")} />
 
             ) : (
                 <FaMoon className="text-xl cursor-pointer hover:text-amber-500" onClick={() => setTheme("dark")} />
-            )
+            ))
             }
 
         </>
