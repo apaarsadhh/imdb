3d8167a22d64d0cf08e930300a32a4a2d4003c45
Apaar <a.sadhh02@gmail.com>
2024-01-29 23:53:52 +0530

    Fetching from TMDB API

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,24 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { metadata } from './layout'
+import Results from "./components/Results";
+
+const API_KEY = process.env.API_KEY
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(` https://api.themoviedb.org/3/${genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"}?api_key=${API_KEY}&language=en-US`, { next: { revalidate: 1000 } });
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  const data = await res.json();
+
+  const results = data.results;
 
 
-export default function Home() {
+
   return (
-    <h1 className="text-red-400"></h1>
+    <div>
+      <Results results={results} />
+    </div>
   )
 }
